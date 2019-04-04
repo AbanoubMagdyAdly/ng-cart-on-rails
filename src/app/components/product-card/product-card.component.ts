4af6cfd7871ca3d81bd903444d0db505318cd757
Hussein El-Attar <hussien.elattar1@gmail.com>
2019-04-04 23:31:19 +0200

    Add product model and component

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-card',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
+@Input('product') product: {};
 
   constructor() { }
 
   ngOnInit() {
   }
-
+ 
 }
---
+++ src/app/models/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    name:string,
+    description:string,
+    picUrl:string,
+    price:number
+}
