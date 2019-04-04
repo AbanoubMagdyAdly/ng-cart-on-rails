ed4863336cc7e689734d0189e282026f94f32e56
Hussein El-Attar <hussien.elattar1@gmail.com>
2019-04-04 19:57:20 +0200

    Add app initial file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartMiniComponent } from './components/cart-mini/cart-mini.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProductComponent } from './pages/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductCardComponent,
+    ProductDetailsComponent,
+    CartMiniComponent,
+    CartComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
