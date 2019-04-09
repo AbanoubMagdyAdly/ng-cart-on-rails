ee4aedc88d44a87c937448056db699f7e77260fa
Hussein El-Attar <hussien.elattar1@gmail.com>
2019-04-09 19:47:55 +0200

    Update app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', redirectTo:'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'cart', component: CartComponent},
 ];
 
 @NgModule({
