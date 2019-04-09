f0ef59de312cadc82aa457a443ea4fda29f781fd
Hussein El-Attar <hussien.elattar1@gmail.com>
2019-04-10 00:30:22 +0200

    Update header with auth service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public email:string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private auth:AuthService) {
   }
 
+  isLoggedIn(){
+    return this.auth.IsAuthenticated();
+  }
+
+  onLogout(){
+    this.auth.logout();
+    this.router.navigate['/home'];
+  }
   ngOnInit() {
   }
 }
