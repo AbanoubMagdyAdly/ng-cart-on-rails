2b3ca2c4f2ec8c930ca440e01f8a9b70793369fd
Hussein El-Attar <hussien.elattar1@gmail.com>
2019-04-09 17:49:27 +0200

    Add cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Product } from 'src/app/models/product';
+import { CartProduct } from 'src/app/models/cart';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  cart:Array<CartProduct>;
+  constructor(private cartService: CartService) { }
 
   ngOnInit() {
+    this.cartService.getCart().subscribe(cart => {
+      this.cart = cart;
+    });
+  }
+  
+  addToCart(product:Product){
+    this.cartService.manipulateCart(product,true);
+  }
+
+  removeOneFromCart(product:Product){
+    this.cartService.manipulateCart(product,false);
+  }
+
+  removeFromCart(product:Product){
+    this.cartService.removeFromCart(product);
   }
 
 }
