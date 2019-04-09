62539aec4c0cb3176fd06d6d700906900c32051b
Hussein El-Attar <hussien.elattar1@gmail.com>
2019-04-09 19:50:13 +0200

    Add product index to product-card

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { CartService } from 'src/app/services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
@@ -8,12 +9,17 @@ import { CartService } from 'src/app/services/cart.service';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
-@Input('product') product: {};
+  @Input('product') product: {};
+  @Input('id') id:number;
 
-  constructor(private cartService: CartService) { 
+  constructor(private cartService: CartService,private router: Router) { 
   }
 
   ngOnInit() {
+  }
+
+  showProduct(id:number) {
+    this.router.navigate(["/products", id]);
   }
   
   onAddToCart(product: Product) {
