ae11701e507dd2581890208d118c2e8732f56d1b
Hussein El-Attar <hussien.elattar1@gmail.com>
2019-04-10 03:09:31 +0200

    Update cart service in single product page

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/services/products.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,11 +14,17 @@ export class ProductDetailsComponent implements OnInit {
   public id:number;
   public product:Product;
 
-  constructor(private route: ActivatedRoute, private prodcutService: ProductsService) { }
+  constructor(private route: ActivatedRoute, private prodcutService: ProductsService, private cartService: CartService) { }
 
+  onAddToCart(product: Product) {
+    this.cartService.manipulateCart(product, true);
+  }
+  
   ngOnInit() {
     this.id = this.route.params['value'].id;
     this.product = this.prodcutService.getSingleProduct(this.id);
   }
+
+  
 
 }
