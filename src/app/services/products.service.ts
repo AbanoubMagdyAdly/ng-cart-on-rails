f85d356d3b7b50474cea9e243aecc58816dcb866
nour <nour.kilany88@gmail.com>
2019-05-05 18:52:12 +0200

    Modify product image url

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -11,8 +11,8 @@ export class ProductsService {
 
   private products: BehaviorSubject<Product[]> = new BehaviorSubject([]);
   private product: BehaviorSubject<Product> = new BehaviorSubject(null);
-  private apiIndex = "http://localhost:3000/products";
-  private apiShow = "http://localhost:3000/products/";
+  private apiIndex = 'http://localhost:3000/products';
+  private apiShow = 'http://localhost:3000/products/';
 
   constructor(private http: HttpClient) {
     this.http
@@ -20,29 +20,29 @@ export class ProductsService {
       .subscribe(products => {
         products = products.map(product => this.mapProduct(product));
         this.products.next(products);
-      })
+      });
   }
 
-  getProducts(): BehaviorSubject<Product[]> {return this.products}
+  getProducts(): BehaviorSubject<Product[]> {return this.products; }
 
   getSingleProduct(id: number): BehaviorSubject<Product> {
     this.http
       .get<Product>(this.apiShow + id)
       .subscribe(product => {
-        return this.product.next(this.mapProduct(product))
-      })
+        return this.product.next(this.mapProduct(product));
+      });
     return this.product;
   }
 
   private mapProduct(product): Product {
     return {
-      "id": product['id'],
-      "title": product['title'],
-      "brand": product['brand'],
-      "category": product['category'],
-      "price": product['price'],
-      "images": product['product_images'].map((pic) => pic.url),
-      "description": product['description']
+      id: product.id,
+      title: product.title,
+      brand: product.brand,
+      category: product.category,
+      price: product.price,
+      images: product.product_images.map((pic) => `http://localhost:3000/${pic.url}`),
+      description: product.description
     };
   }
 }
