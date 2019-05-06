93bb7643442eda9ecfac888eb8ecad65506822f5
nour <nour.kilany88@gmail.com>
2019-05-06 18:35:02 +0200

    Refactor onAddToCart method

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -15,17 +15,15 @@ export class ProductComponent {
     this.cartService.addCartRequest(product, true)
     .subscribe(res => {
         console.log(res);
-        if (res.status === 200) {
-          this.toastr.successToastr(`${product.id} was added to cart `, null,
+        this.toastr.successToastr(`${product.id} was added to cart `, null,
           { animate: 'fade', toastTimeout: 2500, showCloseButton: true });
-          this.cartService.manipulateCart(product, true);
-        } else if (res.status === 422) {
-          console.log('hi');
-        } else if (res.status < 200) {
-          this.toastr.errorToastr(`${product.title} is already in your cart`, null,
+
+        this.cartService.manipulateCart(product, true);
+      }, err => {
+        console.table(err, err.status);
+        this.toastr.errorToastr(`${product.title} is already in your cart`, null,
             { animate: 'fade', toastTimeout: 2500, showCloseButton: true });
-        }
-      }, err => console.log(err.status));
+      });
   }
 
   addToWishlist(product: Product) {
